7d12256d08fcf7274902307e7edd65f3cd0e4e89
Daniel Oloruntoba <danieloloruntoba681@gmail.com>
2024-01-18 09:21:19 +0100

    chore: added data to chart.js

--- src/partials/dashboard/DashboardCard06.jsx
+++ src/partials/dashboard/DashboardCard06.jsx
@@ -1,28 +1,29 @@
-import React from 'react';
-import DoughnutChart from '../../charts/DoughnutChart';
+import React from "react";
+import DoughnutChart from "../../charts/DoughnutChart";
 
 // Import utilities
-import { tailwindConfig } from '../../utils/Utils';
+import { tailwindConfig } from "../../utils/Utils";
 
 function DashboardCard06() {
-
   const chartData = {
-    labels: ['United States', 'Italy', 'Other'],
+    labels: ["United States", "Italy", "Nigeria", "United Kingdom", "Others"],
     datasets: [
       {
-        label: 'Top Countries',
-        data: [
-          35, 30, 35,
-        ],
+        label: "Top Countries",
+        data: [30, 15, 25, 20, 10],
         backgroundColor: [
           tailwindConfig().theme.colors.indigo[500],
           tailwindConfig().theme.colors.blue[400],
           tailwindConfig().theme.colors.indigo[800],
+          tailwindConfig().theme.colors.red[800],
+          tailwindConfig().theme.colors.green[800],
         ],
         hoverBackgroundColor: [
           tailwindConfig().theme.colors.indigo[600],
           tailwindConfig().theme.colors.blue[500],
           tailwindConfig().theme.colors.indigo[900],
+          tailwindConfig().theme.colors.red[900],
+          tailwindConfig().theme.colors.green[900],
         ],
         borderWidth: 0,
       },
@@ -32,7 +33,9 @@ function DashboardCard06() {
   return (
     <div className="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
       <header className="px-5 py-4 border-b border-slate-100 dark:border-slate-700">
-        <h2 className="font-semibold text-slate-800 dark:text-slate-100">Top Countries</h2>
+        <h2 className="font-semibold text-slate-800 dark:text-slate-100">
+          Top Countries
+        </h2>
       </header>
       {/* Chart built with Chart.js 3 */}
       {/* Change the height attribute to adjust the chart height */}
